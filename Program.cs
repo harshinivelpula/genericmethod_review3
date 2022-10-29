c3f512231b39d6b15e50971daefc6c2987a86f20
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 11:14:15 +0530

    generic method example

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace genericreview3
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            bool isequal = calculator.areequal(40, 40);
+            if(isequal)
+            {
+                Console.WriteLine("both are equal");
+            }
+            else
+            {
+                Console.WriteLine("both are not equal");
+            }
+            Console.ReadKey();
+        }
+    }
+    public class calculator
+    {
+       public static bool areequal(int value1, int value2)
+        {
+           return value1 == value2;
+        }
+
+        public static bool areequal<T>(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
